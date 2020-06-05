4660123c28c19b6b223b51a35c139ee7aca2be53
fracariprog <wesleyfracari1@gmail.com>
2020-06-04 22:09:10 -0300

    implements server js and nunjucks

---
+++ src/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const server = express();
+const nunjucks = require("nunjucks");
+
+server.use(express.static("public"));
+
+server.get("/", (req, res) => {
+    return res.render("index.html");
+});
+
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html");
+});
+
+server.get("/search", (req, res) => {
+    return res.render("search-results.html");
+});
+
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true,
+})
+
+server.listen(3000);
